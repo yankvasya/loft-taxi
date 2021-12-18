f745c665881701b94765add2f1b3be3953d6ad27
yankvasya <yankvasya@gmail.com>
2021-12-18 23:55:43 +0300

    feat(card): added styles for card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,26 @@
 import './style.scss';
+import {ReactComponent as Logo}  from "../../assets/logo/logo.svg";
+import {ReactComponent as Chip}  from "../../assets/icons/chip.svg";
+import {ReactComponent as Ellipse}  from "../../assets/icons/ellipse.svg";
 
 const Card = () => {
 
     return (
         <div className="card">
-
+            <div className="card__row">
+                <Logo className="card__logo" />
+                <div className="card__data">
+                    05/08
+                </div>
+            </div>
+            <div className="card__number">5545 2300 3432 4521</div>
+            <div className="card__row">
+                <Chip className="card__chip" />
+                <div className="card__mastercard">
+                    <Ellipse />
+                    <Ellipse />
+                </div>
+            </div>
         </div>
     )
 }
