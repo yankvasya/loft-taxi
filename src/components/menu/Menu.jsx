7c1c6d3b62469155a8fb422713bc84b1387b34ed
yankvasya <yankvasya@gmail.com>
2021-12-19 20:11:10 +0300

    refactor(menu.jsx): simplified duplicate list items

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,15 @@
 import './style.scss'
 
 const Menu = ({currentPage, setCurrentPage}) => {
+    const pages =
+        [
+            { title: 'Карта', name: 'Map' },
+            { title: 'Профиль', name: 'Profile' },
+            { title: 'Выйти', name: 'Auth' }
+        ]
+
+
+
     const goToPage = (e, page) => {
         e.preventDefault()
         setCurrentPage(page)
@@ -8,30 +17,17 @@ const Menu = ({currentPage, setCurrentPage}) => {
 
     return (
         <ul className="menu">
-           <li className="menu__item">
-               <button className={currentPage === 'Map' ? 'menu__link active' : 'menu__link'}
-                       onClick={e => goToPage(e, 'Map')}
-                       disabled={currentPage === 'Map'}
-               >
-                   Карта
-               </button>
-           </li>
-            <li className="menu__item">
-                <button className={currentPage === 'Profile' ? 'menu__link active' : 'menu__link'}
-                        onClick={e => goToPage(e, 'Profile')}
-                        disabled={currentPage === 'Profile'}
-                >
-                    Профиль
-                </button>
-            </li>
-            <li className="menu__item">
-                <button className={currentPage === 'Logout' ? 'menu__link active' : 'menu__link'}
-                        onClick={e => goToPage(e, 'Auth')}
-                        disabled={currentPage === 'Logout'}
-                >
-                    Выйти
-                </button>
-            </li>
+            {pages.map(({ name,title }) => (
+                <li className="menu__item" key={name}>
+                    <button
+                        className={`menu__link ${currentPage === name && 'active' }`}
+                        onClick={e => goToPage(e, name)}
+                        disabled={currentPage === name}
+                    >
+                        {title}
+                    </button>
+                </li>
+            ))}
         </ul>
     )
 }
