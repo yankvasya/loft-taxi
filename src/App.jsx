27d66b639fc373cc37d4c125b9a12278e6f9edba
yankvasya <yankvasya@gmail.com>
2022-01-15 00:19:58 +0300

    feat(privateroute): created and used PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import Auth from './pages/Auth/Auth'
 import { Profile } from './pages/Profile/Profile'
 import { Map } from './pages/Map/Map'
 import { connect } from 'react-redux'
-// import { PrivateRoute } from './PrivateRoute'
+import PrivateRoute from './PrivateRoute'
 
-function App ({ isLoggedIn }) {
+function App () {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Auth />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/map" element={<PrivateRoute><Map /></PrivateRoute>} />}
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
       </Routes>
     </div>
   )
